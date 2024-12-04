159d2d812bee9e31af76c2753ea23c245db025d0
kei <143186747+keiciraptor@users.noreply.github.com>
2024-12-04 10:42:33 +0800

    Update mainmenu.java

--- src/neurocrusade/mainmenu.java
+++ src/neurocrusade/mainmenu.java
@@ -7,7 +7,53 @@ package neurocrusade;
 /**
  *
  * @author Kei Pasaol
+ * @author KJ Ares
  */
-public class mainmenu {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainmenu implements MouseListener{
+    JFrame frame;
+    JLabel play;
+    ImageIcon playIcon;
+    JLabel mechanics;
+    ImageIcon mechanicsIcon;
+    JLabel credits;
+    ImageIcon creditsIcon;
+    JLabel settings; 
+    ImageIcon settingsIcon;
+    JLabel music;
+    ImageIcon musicIcon;
+    ImageIcon gamebgIcon;
+    ImageIcon img;
+    //place all other elements in the background too, yuh
+    //PlaySound click;
+    public mainmenu()
+    {
+        //click = newPlaySound();
+        
+        frame = new JFrame("NeuroCrusade");
+        
+        gamebgIcon = new ImageIcon("Images/mainmenu.png");
+        //set
+        playIcon = new ImageIcon("Images/play.png");
+        //set
+        mechanicsIcon = new ImageIcon("Images/mech.png");
+        //set
+        creditsIcon = new ImageIcon("Images/creds.png");
+        //set
+        settingsIcon = new ImageIcon("Images/settings.png");
+        //set
+        musicIcon = new ImageIcon("Images/music.png");
+        //set
+        
+    }
     
+    public void setFrame()
+    {
+        frame.setLayout(new GraphPaperLayout(new Dimension(27,18)));
+       
+    }
 }
