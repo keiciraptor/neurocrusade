e5b67bdd18d6ea687f5c2b2f61fea9c8f45d8761
kei <143186747+keiciraptor@users.noreply.github.com>
2024-12-04 10:09:23 +0800

    Update mainmenu.java

--- neurocrusade/mainmenu.java
+++ neurocrusade/mainmenu.java
@@ -7,7 +7,24 @@ package neurocrusade;
 /**
  *
  * @author Kei Pasaol
+ * @author KJ Ares
  */
-public class mainmenu {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainmenu implements ActionListener{
+    JFrame frame;
+    JButton play;
+    JButton mechanics;
+    JButton credits;
     
+    public mainmenu()
+    {
+        frame = new JFrame("NeuroCrusade");
+        play = new JButton("Play");
+        mechanics = new JButton("Mechanics");
+        credits = new JButton("Credits");
+    }
 }
